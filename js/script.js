6a81b76f6e314c148c7b4e952a9c930c59ea140f
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-03-18 15:43:19 +0100

    random bomb completed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,40 @@
-// <div class="square-easy"></div>
+let container = document.getElementById('container')
+
 function generateNewElementGrid(content){
     // creo un elemento del tipo <div>...</div>
     let newElement = document.createElement('div');
     newElement.classList.add('square-easy');
     return newElement;
+}
+
+// codice gioco perso
+let list = [];
+// list.push(generateUniqueRandomNumber(1, 100, list));
+// console.log(list)
+
+
+function getRandomInt(minimum, maximus){
+    return Math.floor(Math.random() * (maximus - minimum + 1) ) + minimum;
+}
+
+function generateUniqueRandomNumber(min, max, blacklist){
+    let isFound = false;
+    let randomNumber;
+
+    while(isFound === false){
+        randomNumber = getRandomInt(min, max);
+        if(blacklist.includes(randomNumber) === false){
+            isFound = true
+        }
+    }
+    return randomNumber;
+}
+
+// funzione termine gioco
+function lose(){
+    const lose = document.createElement('div');
+    lose.className = 'game-end';
+    grid.append(lose);
 }
 
 let play = document.getElementById('play');
@@ -12,68 +43,76 @@ play.addEventListener('click', function(){
     let difficulty = document.getElementById('difficulty').value;
     let grid = document.getElementById('grid');
     grid.innerHTML = ''
-    let rBtnN = document.getElementById('reset-btn')
     grid.classList.add('d-block');   
-    rBtnN.classList.add('d-flex');
     let gridElement = document.getElementById('grid');
     //creo ciclo for per inserire i quadrati nella grid
     if(difficulty == 'hard'){
+        for(let i = 0; i < 16; i++){
+            list.push(generateUniqueRandomNumber(1, 49, list))
+            console.log(list);
+        }
         for (let i = 0; i < 49; i++) {
             let newSquare = generateNewElementGrid()
             newSquare.classList.add('hard')
             // aggiungo quadrato alla grid
             gridElement.appendChild(newSquare);
-            console.log('ok')
             // coloro di azzurro quando clicco il quadrato
             newSquare.addEventListener('click', function(){
                 newSquare.classList.add('square-click')
-                console.log(`Hai selezionato la casella numero: ${i + 1}`);    
-            })
+                console.log(`Hai selezionato la casella numero: ${i + 1}`);
+
+                if(list.includes(i + 1)){
+                    console.log('Hai perso')
+                    newSquare.classList.add('bomb-red');
+                    lose(true);
+                }  
+            });
     
-            let resetButton = document.getElementById('reset');
-            resetButton.addEventListener('click', function(){
-                newSquare.classList.remove('square-click');
-                console.log('Gioco Ricominciato')
-            })
         }
     } else if(difficulty == 'medium'){
+        for(let i = 0; i < 16; i++){
+            list.push(generateUniqueRandomNumber(1, 49, list))
+            console.log(list);
+        }
         for (let i = 0; i < 81; i++) {
             let newSquare = generateNewElementGrid()
             newSquare.classList.add('medium')
             // aggiungo quadrato alla grid
             gridElement.appendChild(newSquare);
-            console.log('ok')
             // coloro di azzurro quando clicco il quadrato
             newSquare.addEventListener('click', function(){
                 newSquare.classList.add('square-click')
-                console.log(`Hai selezionato la casella numero: ${i + 1}`);    
-            })
+                console.log(`Hai selezionato la casella numero: ${i + 1}`);
+                
+                if(list.includes(i + 1)){
+                    console.log('Hai perso')
+                    newSquare.classList.add('bomb-red');
+                    lose(true)
+                }
+            });
     
-            let resetButton = document.getElementById('reset');
-            resetButton.addEventListener('click', function(){
-                newSquare.classList.remove('square-click');
-                console.log('Gioco Ricominciato')
-            })
         }
     } else {
+        for(let i = 0; i < 16; i++){
+            list.push(generateUniqueRandomNumber(1, 100, list));
+            console.log(list)
+        }
         for (let i = 0; i < 100; i++) {
             let newSquare = generateNewElementGrid()
             // aggiungo quadrato alla grid
             gridElement.appendChild(newSquare);
-            console.log('ok')
             // coloro di azzurro quando clicco il quadrato
             newSquare.addEventListener('click', function(){
-                newSquare.classList.add('square-click')
+                newSquare.classList.add('square-click');
+                
+                if(list.includes(i + 1)){
+                    console.log('Hai perso')
+                    newSquare.classList.add('bomb-red');
+                    lose(true)
+                }
                 console.log(`Hai selezionato la casella numero: ${i + 1}`);    
             })
     
-            let resetButton = document.getElementById('reset');
-            resetButton.addEventListener('click', function(){
-                newSquare.classList.remove('square-click');
-                console.log('Gioco Ricominciato')
-            })
         }
     }
 })
-
-// creo una variabile per assegnare dove mettere i quadrati
